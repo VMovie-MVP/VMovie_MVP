af2090dc594f807cf9027abbe958e636581007c9
sunshine-zhuzhu <1820703199@qq.com>
2017-04-10 18:56:54 +0800

    init

---
+++ app/src/main/java/com/example/sunshine/vmovie2/ui/home/ChanelListFragment.java
@@ -0,0 +1,27 @@
+package com.example.sunshine.vmovie2.ui.home;
+
+import android.support.v4.app.Fragment;
+import com.example.mvplibrary.base.BaseFragment;
+import com.example.sunshine.vmovie2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ChanelListFragment extends BaseFragment {
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_chanel_list;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initPresenter() {
+
+    }
+}
